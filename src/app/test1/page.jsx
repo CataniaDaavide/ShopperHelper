80fe99924856a0d033dd8d43b1822d507fbe5a9e
Davide Catania <catania561@gmail.com>
2025-11-17 17:09:09 +0100

    update

--- src/app/test1/page.jsx
+++ src/app/test1/page.jsx
@@ -1,8 +1,22 @@
-export default function Page(){
-    return(
-        <div className="w-full h-full flex flex-col items-center justify-between">
+export default function Page() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-between">
       <div className="w-full h-20 bg-yellow-500"></div>
-      <p>page w-full h-full</p>
-      <div className="w-full h-20 bg-yellow-500"></div>        </div>
-    )
+      <div className="flex-1 w-full flex flex-col gap-3 overflow-y-auto">
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+        <div className="bg-green-300 w-full min-h-20"></div>
+      </div>
+      <div className="w-full h-20 bg-yellow-500"></div>{" "}
+    </div>
+  );
 }
